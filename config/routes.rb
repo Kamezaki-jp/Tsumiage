157f6340279be6af2cde2f943ee2785ba80e966b
Kamezaki-jp <champagne.es335@gmail.com>
2021-04-05 06:12:33 +0000

    改善：リファクタリング#3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
-  post '/homes/guest_sign_in', to: 'homes#new_guest'
+  devise_scope :user do
+    post '/users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 end
