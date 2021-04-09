792b2b30538b4c11b2c3f859a831280987ac8bde
Kamezaki-jp <champagne.es335@gmail.com>
2021-04-09 10:51:46 +0000

    修正：各種ページのレイアウト修正#20

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,10 +3,16 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # destroyアクションの前だけゲストログインされているか確認
   before_action :check_guest, only: %i[update destroy]
-  
+
   def check_guest
     if resource.email == 'guest@example.com'
       redirect_to root_path, alert: 'ゲストユーザーの編集・削除はできません。'
     end
   end
+
+  protected
+
+    def after_update_path_for(resource)
+      edit_user_registration_path
+    end
 end
