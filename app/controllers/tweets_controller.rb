245189e89450262bd2159c1d175c06fc01a7eb1a
Kamezaki-jp <champagne.es335@gmail.com>
2021-04-10 07:53:09 +0000

    修正：UserとTaskのアソシエーションを修正#10

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,38 +2,35 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
-  
+
   def new
     @tweet = Tweet.new
-    # @tweet.tasks.build
+    @tweet.tasks.build
   end
-  
+
   def create
     @tweet = Tweet.new(tweet_params)
     @tweet.user_id = current_user.id
-    
-    if @tweet.save
-      redirect_to tweets_path
-    else
-      render 'new', alert: '必須項目'
-    end
+    @tweet.save
+    redirect_to tweets_path
   end
-  
+
   def show
-    
+    @tweet = Tweet.find(params[:id])
+    @user = @tweet.user
   end
-  
+
   def update
-    
+
   end
-  
+
   def destroy
-    
+
   end
-  
+
   private
-  
+
     def tweet_params
-      params.require(:tweet).permit(:body, tasks_attributes: [:id, :body,:done, :_destroy])
+      params.require(:tweet).permit(:body, tasks_attributes: [:id, :task_name, :done, :_destroy])
     end
 end
