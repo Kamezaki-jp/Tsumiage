ba418577e5f821be207990edc7e2335e6d88ab04
Kamezaki-jp <champagne.es335@gmail.com>
2021-04-15 12:16:56 +0000

    追加：総合ランキングのデータ取得#31

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,8 @@
 class HomesController < ApplicationController
   def top
   end
+
+  def users_ranking
+    @users = User.joins(tweets: :tasks).where(tasks: {status: 2}).group("users.name").order('count(tasks.status) desc')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#top'
+  get 'homes/users_ranking' => 'homes#users_ranking'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
@@ -7,14 +8,13 @@ Rails.application.routes.draw do
   devise_scope :user do
     post '/users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-  
+
   post 'follow/:id'         => 'relationships#follow',    as: 'follow'
   post 'unfollow/:id'       => 'relationships#unfollow',  as: 'unfollow'
   get  'user/:id/follows'   => 'relationships#follows',   as: 'follows'
   get  'user/:id/followers' => 'relationships#followers', as: 'followers'
-  
   get  '/search'            => 'searches#search'
-  
+
   resources :users
   resources :tweets do
     resource :cheers, only: [:create, :destroy]
