2c4626649ffd90cea4ccd584b45ce1a4ebd6a9a3
Kamezaki-jp <champagne.es335@gmail.com>
2021-04-20 13:11:51 +0000

    改善：タスク完了時に経験値が重複しない#39

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -16,7 +16,7 @@ class TweetsController < ApplicationController
     if @tweet.save
       redirect_to tweet_path(@tweet), notice: "投稿しました。"
     else
-      render action: :new
+      render 'new', alert: "必須項目があります"
     end
   end
 
@@ -33,33 +33,19 @@ class TweetsController < ApplicationController
 
   def update
     @tweet = Tweet.find(params[:id])
-    @task = @tweet.tasks
     @user = User.find(@tweet.user_id)
-
-    # Tweetに含まれるタスクが一つでも完了している場合はTweetを消せない。
-    # Taskが完了している場合は該当Taskは消せない。
-    # Taskが完了している場合は該当Taskは完了以外に出来ない。
-    # Viewで上記の仕様もUserに伝えられるように変えておくと共にControllerで制御してください。
-
-    # 配列化
+    
+    # editのフォームから受け取ったデータを配列にする
     tmp = []
-    tweet_params[:tasks_attributes].each {|k, v| tmp[k.to_i] = v}
-    tmp.each do |task|
-      if Task.find_by(id: task[:id], status:2) != nil # すでに完了済みのTask
-        if task[:status] != "完了"  # ユーザがフォームで完了以外のパラメータを指定した場合
-          #   エラー
-          return
+    tweet_params[:tasks_attributes].each {|k, v|
+        if v["task_name"].present?
+            tmp[k.to_i] = v
         end
-      end
-    end
+    }
 
     if @tweet.update(tweet_params)
-      #更新した内容にタスクのステータスが２の完了が含まれるとき
-      #レベル上限は100
-
-      # tasksをTaskテーブルからtweet_params[:tasks_attributes]に含まれるもののみに限定する。
-      # tweet_params[:tasks_attributes]に複数タスクがあった場合ループでIDを取得して該当レコードが完了であればLevel_upを呼ぶ。
-      tmp.each do |task|
+      # 配列からnilと空文字を取り除く
+      tmp.compact.reject(&:empty?).each do |task|
         @task = Task.find_by(id: task[:id], status:2)
         if @task != nil && @user.level < 100
           level_up
@@ -67,21 +53,12 @@ class TweetsController < ApplicationController
       end
       redirect_to tweet_path(@tweet), notice: "更新しました。"
     else
-      render 'tweets/edit', alert: "必須項目があります。"
+      render 'edit', alert: "必須項目があります。"
     end
   end
 
   def destroy
-    @tweet = Tweet.find(params[:id])
-    @task = @tweet.tasks
-
-    # @task.each do |task|
-    #   if task.status == 2
-    #     #エラー
-    #   end
-    # end
-
-    @tweet.destroy
+    Tweet.find(params[:id]).destroy
     redirect_to user_path(current_user), alert: '投稿を削除しました。'
   end
 
@@ -95,19 +72,14 @@ class TweetsController < ApplicationController
     def level_up
       #変数に現在のユーザーの経験値を代入
       totalExp = @user.experience_point
-      #得られた経験値をユーザーの経験値に加算
       #１タスク１０ｐｔで設定
-      totalExp += 10 #@task.where(status: 2).count * 10
-      #加算した経験値をuserの総経験値を示す変数に再代入
+      totalExp += 10
       @user.experience_point = totalExp
-      #更新の処理
       @user.update(experience_point: totalExp)
-      #レベルセッティングのモデルから、今の自分のレベルより1高いレコードを探し代入
+      # レベルアップの判定
       levelSetting = LevelSetting.find_by(level: @user.level + 1);
-
-      #探してきたレコードの閾値よりもユーザーの経験値が高かった場合
+      
       if levelSetting.thresold <= @user.experience_point
-        #レベルを1増やして更新
         @user.level = @user.level + 1
         @user.update(level: @user.level)
       end
